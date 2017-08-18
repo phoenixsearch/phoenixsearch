0afa4172f743d7f636e14b2c8f7ff6b237501053
Kushmantsev Arthur <zeusakm@gmail.com>
2017-08-18 15:04:28 +0300

    *	#22. fix info store_size

--- src/core/Info.php
+++ src/core/Info.php
@@ -8,6 +8,7 @@ use Predis\Client;
 
 /**
  * Trait Info
+ *
  * @package pheonixsearch\core
  *
  * @property Client redisConn
@@ -17,39 +18,59 @@ trait Info
     /**
      *
      * @param StdFields $stdFields
+     *
      * @return bool
      */
     public function setInfo(StdFields $stdFields): bool
     {
-        $index = $stdFields->getIndex();
-        $data  = [
+        $index      = $stdFields->getIndex();
+        $data       = [
             IndexInterface::INDEX       => $index,
             InfoInterface::DOCS_COUNT   => 1,
             InfoInterface::DOCS_DELETED => 0,
-            InfoInterface::STORE_SIZE   => InfoInterface::DEFAULT_USED_MEMORY,
         ];
-        $hashData = $this->redisConn->hget(InfoInterface::INFO_INDICES, $index);
+        $systemInfo = [
+            InfoInterface::STORE_SIZE => InfoInterface::DEFAULT_USED_MEMORY,
+        ];
+        $hashData   = $this->redisConn->hget(InfoInterface::INFO_INDICES, $index);
         if (empty($hashData) === false) {
-            $d = $this->unser($hashData);
-            $d[InfoInterface::DOCS_COUNT] = ++$d[InfoInterface::DOCS_COUNT];
-            $info = $this->redisConn->info();
-            $d[InfoInterface::STORE_SIZE] = $info[InfoInterface::MEMORY][InfoInterface::INFO_USED_MEMORY];
+            $d                                     = $this->unser($hashData);
+            $d[InfoInterface::DOCS_COUNT]          = ++$d[InfoInterface::DOCS_COUNT];
+            $info                                  = $this->redisConn->info();
+            $systemInfo[InfoInterface::STORE_SIZE] = $info[InfoInterface::MEMORY][InfoInterface::INFO_USED_MEMORY];
             $this->redisConn->hset(InfoInterface::INFO_INDICES, $index, $this->ser($d));
+            $this->redisConn->hset(
+                InfoInterface::INFO_INDICES, InfoInterface::SYSTEM_INFO, $this->ser(
+                [
+                    InfoInterface::STORE_SIZE => $info[InfoInterface::MEMORY][InfoInterface::INFO_USED_MEMORY],
+                ]
+            )
+            );
+
             return false;
         }
+        $this->redisConn->hset(InfoInterface::INFO_INDICES, InfoInterface::SYSTEM_INFO, $this->ser($systemInfo));
         $this->redisConn->hset(InfoInterface::INFO_INDICES, $index, $this->ser($data));
+
         return true;
     }
 
     public function decrInfo(StdFields $stdFields)
     {
-        $index = $stdFields->getIndex();
-        $hashData = $this->redisConn->hget(InfoInterface::INFO_INDICES, $index);
-        $d = $this->unser($hashData);
+        $index                          = $stdFields->getIndex();
+        $hashData                       = $this->redisConn->hget(InfoInterface::INFO_INDICES, $index);
+        $d                              = $this->unser($hashData);
         $d[InfoInterface::DOCS_DELETED] = ++$d[InfoInterface::DOCS_DELETED];
         $d[InfoInterface::DOCS_COUNT]   = --$d[InfoInterface::DOCS_COUNT];
-        $info = $this->redisConn->info();
-        $d[InfoInterface::STORE_SIZE] = $info[InfoInterface::MEMORY][InfoInterface::INFO_USED_MEMORY];
+        $info                           = $this->redisConn->info();
+        $d[InfoInterface::STORE_SIZE]   = $info[InfoInterface::MEMORY][InfoInterface::INFO_USED_MEMORY];
+        $this->redisConn->hset(
+            InfoInterface::INFO_INDICES, InfoInterface::SYSTEM_INFO, $this->ser(
+            [
+                InfoInterface::STORE_SIZE => $info[InfoInterface::MEMORY][InfoInterface::INFO_USED_MEMORY],
+            ]
+        )
+        );
         $this->redisConn->hset(InfoInterface::INFO_INDICES, $index, $this->ser($d));
     }
 
@@ -59,6 +80,7 @@ trait Info
         foreach ($indicesInfo as $k => $val) {
             $indicesInfo[$k] = $this->unser($val);
         }
+
         return $indicesInfo;
     }
 }
--- src/types/InfoInterface.php
+++ src/types/InfoInterface.php
@@ -12,6 +12,7 @@ namespace pheonixsearch\types;
 interface InfoInterface
 {
     const INFO_INDICES        = 'info:indices';
+    const SYSTEM_INFO         = 'system_info';
     const DOCS_COUNT          = 'docs_count';
     const DOCS_DELETED        = 'docs_deleted';
     const STORE_SIZE          = 'store_size';
