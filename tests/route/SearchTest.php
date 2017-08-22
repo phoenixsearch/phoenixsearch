075d08495f020f1565a47204e882092e0ad46b3c
Kushmantsev Arthur <zeusakm@gmail.com>
2017-08-22 09:24:23 +0300

    *	#6. phpunit dependencies

--- tests/route/SearchTest.php
+++ tests/route/SearchTest.php
@@ -8,26 +8,40 @@ class SearchTest extends TestCase
 {
     private $requestHandler = null;
 
-    public function setUpBeforeClass()
+    public static function setUpBeforeClass()
     {
     }
 
     public function testUpdate()
     {
-
     }
 
     public function testSearch()
     {
         $_SERVER['REQUEST_METHOD'] = 'GET';
-        $this->requestHandler = new RequestHandler();
-        $this->requestHandler->setRequestBodyJson('');
+        $_SERVER['REQUEST_URI']    = '/myindex/myindextype?pretty';
+        $this->requestHandler      = new RequestHandler();
+        $json                      = '{   
+    "offset":0, 
+    "limit":10, 
+    "highlight" : {
+        "pre_tags" : ["<tag1>", "<tag2>"],
+        "post_tags" : ["</tag1>", "</tag2>"],
+        "fields" : {
+            "name" : {}, "text" : {}
+        }
+    },
+    "query" : {
+        "term" : { "text" : "Lorem ipsum" }
+    }
+}';
+        $this->requestHandler->setRequestBodyJson($json);
+        $this->requestHandler->setRequestBodyArray(json_decode($json, true));
         $search = new Search($this->requestHandler);
         $search->performSearch();
     }
 
     public function testDelete()
     {
-
     }
 }
